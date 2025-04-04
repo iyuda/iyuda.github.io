f4c2f12955047cbd19a11c54c2d800a81c8fcf71
iyuda <iyuda@hotmail.com>
2025-04-04 18:38:04 -0400

    ckear docs

--- docs/server/assets-chunks/styles-LM6VLD64_css.mjs
+++
@@ -1,2 +0,0 @@
-export default `.hero,.skills,.projects,.resume,.contact{text-align:center;padding:20px}.btn{display:inline-block;padding:10px 20px;margin:10px;background:#007acc;color:#fff;text-decoration:none;border-radius:5px}
-`;
