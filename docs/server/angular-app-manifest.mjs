581140ca57e4cf995b07554c9116625ed29b78df
iyuda <iyuda@hotmail.com>
2025-05-16 16:47:14 -0400

    --base-href ./

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,36 +2,36 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/iyuda-portfolio/',
+  baseHref: './',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/iyuda-portfolio"
+    "route": "/"
   },
   {
     "renderMode": 2,
-    "route": "/iyuda-portfolio/skills"
+    "route": "/skills"
   },
   {
     "renderMode": 2,
-    "route": "/iyuda-portfolio/projects"
+    "route": "/projects"
   },
   {
     "renderMode": 2,
-    "route": "/iyuda-portfolio/featured-projects"
+    "route": "/featured-projects"
   },
   {
     "renderMode": 2,
-    "route": "/iyuda-portfolio/resume"
+    "route": "/resume"
   },
   {
     "renderMode": 2,
-    "route": "/iyuda-portfolio/contact"
+    "route": "/contact"
   },
   {
     "renderMode": 2,
-    "route": "/iyuda-portfolio/finance-dashboard"
+    "route": "/finance-dashboard"
   },
   {
     "renderMode": 2,
@@ -40,7 +40,7 @@ export default {
       "chunk-QNAF5OXN.js",
       "chunk-TQTN3ZQO.js"
     ],
-    "route": "/iyuda-portfolio/finance-dashboard/stocks"
+    "route": "/finance-dashboard/stocks"
   },
   {
     "renderMode": 2,
@@ -49,7 +49,7 @@ export default {
       "chunk-QNAF5OXN.js",
       "chunk-TQTN3ZQO.js"
     ],
-    "route": "/iyuda-portfolio/finance-dashboard/stock-data"
+    "route": "/finance-dashboard/stock-data"
   },
   {
     "renderMode": 2,
@@ -58,7 +58,7 @@ export default {
       "chunk-QNAF5OXN.js",
       "chunk-TQTN3ZQO.js"
     ],
-    "route": "/iyuda-portfolio/finance-dashboard/gainers-losers"
+    "route": "/finance-dashboard/gainers-losers"
   },
   {
     "renderMode": 2,
@@ -66,7 +66,7 @@ export default {
       "chunk-ENRXE7Q4.js",
       "chunk-TQTN3ZQO.js"
     ],
-    "route": "/iyuda-portfolio/finance-dashboard/markets"
+    "route": "/finance-dashboard/markets"
   },
   {
     "renderMode": 2,
@@ -75,29 +75,29 @@ export default {
       "chunk-QNAF5OXN.js",
       "chunk-TQTN3ZQO.js"
     ],
-    "route": "/iyuda-portfolio/finance-dashboard/news"
+    "route": "/finance-dashboard/news"
   },
   {
     "renderMode": 2,
-    "route": "/iyuda-portfolio/**"
+    "route": "/**"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 16028, hash: 'b64e8a8bcd89d57f279b19c3d2ca55a24a413840cdcc118adc80bb61d7d8778d', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1588, hash: 'bc6cea9eaace567692e5bf1a50417adcd1546abae07b63ffbbbb0e18ec5068ef', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 29360, hash: '067f0c80739b12b29cfa588113b2b9f6c113e886a5e89d3e22c02a68d1fddbb8', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'featured-projects/index.html': {size: 25233, hash: 'd206210fd50c1a47361e2f841e337ad048278c6a14de40ce45a9cc6f4e2db5cc', text: () => import('./assets-chunks/featured-projects_index_html.mjs').then(m => m.default)},
-    'resume/index.html': {size: 21611, hash: '3820d1bf50dab4b39d862b9782d2c17032184761e1f5728222218fbd0f93e0f2', text: () => import('./assets-chunks/resume_index_html.mjs').then(m => m.default)},
-    'contact/index.html': {size: 22225, hash: '5ee0799fff03c357639e56a251f97868f67e7749db6aeb495e3b9c31c85c3e3e', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
-    'skills/index.html': {size: 21559, hash: '82ddd787e2b6317141c8d88258e51aecfd87bde6dad35b9fef08948fb7eaeadf', text: () => import('./assets-chunks/skills_index_html.mjs').then(m => m.default)},
-    'finance-dashboard/index.html': {size: 21748, hash: 'd86b53a7b1c1277efc88e85223fe1adc9a4735b92d23c30542775e0a43e68367', text: () => import('./assets-chunks/finance-dashboard_index_html.mjs').then(m => m.default)},
-    'finance-dashboard/stock-data/index.html': {size: 23627, hash: '1c3ede2907fc88c4d7f676d690f2a413a1e0b98da513e334f53182a86c5b2f7c', text: () => import('./assets-chunks/finance-dashboard_stock-data_index_html.mjs').then(m => m.default)},
-    'finance-dashboard/stocks/index.html': {size: 23268, hash: 'f0ec69f5eeab540d26195e8c60a9effdb87ae29c4c1413b51b27a8998640bad8', text: () => import('./assets-chunks/finance-dashboard_stocks_index_html.mjs').then(m => m.default)},
-    'projects/index.html': {size: 21667, hash: 'fe587cbe4b6095a658d4c66feeac615abb763ef009890bb70c5b4a64b9820085', text: () => import('./assets-chunks/projects_index_html.mjs').then(m => m.default)},
-    'finance-dashboard/markets/index.html': {size: 21934, hash: '0ba43ee7d4cbbb7f2954c13bfb71713ff57d3357de73dbabc3339e8d61949d49', text: () => import('./assets-chunks/finance-dashboard_markets_index_html.mjs').then(m => m.default)},
-    'finance-dashboard/gainers-losers/index.html': {size: 141981, hash: 'd031a42c88a8a04b0a38104c06ccc1eb497f50dd54becbfc4ad783be2b9b3d1f', text: () => import('./assets-chunks/finance-dashboard_gainers-losers_index_html.mjs').then(m => m.default)},
-    'finance-dashboard/news/index.html': {size: 56410, hash: 'eac1ed96642688f9a9f282e0e286d2f384dc433cab60613cebd05ff88e99fdb4', text: () => import('./assets-chunks/finance-dashboard_news_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 16013, hash: 'edcb7eee7f4a19bfdec34567067f7dccac4fa651f92b99be65379ffe29ffac5e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1573, hash: '4cbfeebdd096486ed54563e97c19077773c264957b98379f87a474bebe356737', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'skills/index.html': {size: 21544, hash: '505e9bf344255d2012fc183fcae45cbc052bd99d77f6677218cf64743d30f0e1', text: () => import('./assets-chunks/skills_index_html.mjs').then(m => m.default)},
+    'featured-projects/index.html': {size: 25218, hash: '839a30915fdfd2b3f6e8ff67fc6f64164aa5ee3cc95305f34140b9c2d1edf1aa', text: () => import('./assets-chunks/featured-projects_index_html.mjs').then(m => m.default)},
+    'resume/index.html': {size: 21596, hash: 'fbf4a03522e867ec70c437b6afcb3080fe9b2e58670e89da2dfeb03a6cbc5997', text: () => import('./assets-chunks/resume_index_html.mjs').then(m => m.default)},
+    'contact/index.html': {size: 22210, hash: '535626b206ff84cd670db9b9a77b01942eb89f0a1b5e44a89f193498ef844bc1', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
+    'finance-dashboard/stocks/index.html': {size: 23253, hash: '4f2ca4a18659c232022824b73ac6017ad1348fb37e30d7fabc1050df5483a0ee', text: () => import('./assets-chunks/finance-dashboard_stocks_index_html.mjs').then(m => m.default)},
+    'finance-dashboard/stock-data/index.html': {size: 23612, hash: '2ab7740a37d860e0baf6ea75a7ce629f6ccc4329d35817823ef7b8e7eba991fd', text: () => import('./assets-chunks/finance-dashboard_stock-data_index_html.mjs').then(m => m.default)},
+    'projects/index.html': {size: 21652, hash: 'a092f66a8159954e6856e1377b43596b09be50ba4059aa5e54036bac3cf71871', text: () => import('./assets-chunks/projects_index_html.mjs').then(m => m.default)},
+    'finance-dashboard/index.html': {size: 21733, hash: '56539f6f2693a7994f654dc740b97c286dc370ad515476aa9173fc5eb1527b5a', text: () => import('./assets-chunks/finance-dashboard_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 29345, hash: 'e9d60cd8ad82fab5303016ebfbbd150587921113f0bccacff7fdbc541d042383', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'finance-dashboard/markets/index.html': {size: 21919, hash: 'cf2ae9a211f7bf3d22daf60471704af95ee04bfddbff67c01a47bc7cac2a311f', text: () => import('./assets-chunks/finance-dashboard_markets_index_html.mjs').then(m => m.default)},
+    'finance-dashboard/news/index.html': {size: 55740, hash: '7dda21fcf3f81b36bb0572642870a35666c6dacfafd4096d104d5b0cffac8b42', text: () => import('./assets-chunks/finance-dashboard_news_index_html.mjs').then(m => m.default)},
+    'finance-dashboard/gainers-losers/index.html': {size: 140455, hash: 'b07c53d45c26193276ac866b275955573e75cefe12adf8fd2b4c06f9bdae1851', text: () => import('./assets-chunks/finance-dashboard_gainers-losers_index_html.mjs').then(m => m.default)},
     'styles-566YWFES.css': {size: 15510, hash: 'Wg7GXb2DHDY', text: () => import('./assets-chunks/styles-566YWFES_css.mjs').then(m => m.default)}
   },
 };
